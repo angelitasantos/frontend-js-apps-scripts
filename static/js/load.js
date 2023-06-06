191e610841f0327844d082cd7d8736414a642644
Angelita Santos <65367322+angelitasantos@users.noreply.github.com>
2023-06-05 22:38:10 -0300

    Add files via upload

---
+++ static/js/load.js
@@ -0,0 +1,7 @@
+// Header
+$("#nav-header").load("../../templates/base/header.html #navegacao");
+
+// Breadcrumb
+$("#nav-breadcrumb-registros").load("../../templates/base/breadcrumb.html #breadcrumb #breadcrumb-registros");
+$("#nav-breadcrumb-financeiro").load("../../templates/base/breadcrumb.html #breadcrumb #breadcrumb-financeiro");
+$("#nav-breadcrumb-cadastros").load("../../templates/base/breadcrumb.html #breadcrumb #breadcrumb-cadastros");
