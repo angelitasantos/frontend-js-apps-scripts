851210f5d1fc8c2a5f8a7e051659d2cb531f8171
Angelita Santos <65367322+angelitasantos@users.noreply.github.com>
2023-06-06 20:22:47 -0300

    Add files via upload

--- static/js/load.js
+++ static/js/load.js
@@ -1,7 +1,7 @@
 // Header
-$("#nav-header").load("../../templates/base/header.html #navegacao");
+$("#nav-header").load("./templates/base/header.html #navegacao");
 
 // Breadcrumb
-$("#nav-breadcrumb-registros").load("../../templates/base/breadcrumb.html #breadcrumb #breadcrumb-registros");
-$("#nav-breadcrumb-financeiro").load("../../templates/base/breadcrumb.html #breadcrumb #breadcrumb-financeiro");
-$("#nav-breadcrumb-cadastros").load("../../templates/base/breadcrumb.html #breadcrumb #breadcrumb-cadastros");
+$("#nav-breadcrumb-registros").load("./templates/base/breadcrumb.html #breadcrumb #breadcrumb-registros");
+$("#nav-breadcrumb-financeiro").load("./templates/base/breadcrumb.html #breadcrumb #breadcrumb-financeiro");
+$("#nav-breadcrumb-cadastros").load("./templates/base/breadcrumb.html #breadcrumb #breadcrumb-cadastros");
