6f7bd410fea478df8ebf2a09da9cc872745885da
Angelita Santos <65367322+angelitasantos@users.noreply.github.com>
2023-07-11 08:56:42 -0300

    Delete static directory

--- static/js/load.js
+++
@@ -1,8 +0,0 @@
-// Header
-$("#nav-header").load("./templates/base/header.html #navegacao");
-$("#nav-navbar").load("../../templates/base/navbar.html #navegacao");
-
-// Breadcrumb
-$("#nav-breadcrumb-registros").load("../../templates/base/breadcrumb.html #breadcrumb #breadcrumb-registros");
-$("#nav-breadcrumb-financeiro").load("../../templates/base/breadcrumb.html #breadcrumb #breadcrumb-financeiro");
-$("#nav-breadcrumb-cadastros").load("../../templates/base/breadcrumb.html #breadcrumb #breadcrumb-cadastros");
